bc2151fd74641bd993e0da225d577d1af7b7505f
mistahoward <me@alexhoward.dev>
2023-07-25 02:19:37 -0500

    init project setup

---
+++ src/game.cpp
@@ -0,0 +1,13 @@
+#include "../include/game.h"
+#include <iostream>
+
+Game::Game()
+{
+    // Constructor implementation (initialize game variables, etc.)
+}
+
+void Game::run()
+{
+    std::cout << "Welcome to The Enigmatic Enclave: Survival Unveiled!" << std::endl;
+    // Main game loop implementation
+}
